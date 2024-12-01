bf65f7f4a89ad639b0f33562b83a35aa6b9836ef
Ivan Morales <IvanJohnMorales@gmail.com>
2024-12-01 16:21:48 -0500

    Changed env variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,13 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDN4b91qetSGMkEvUM8TviG8ZkD5DZ4cSQ",
-  authDomain: "zealthy-test.firebaseapp.com",
-  projectId: "zealthy-test",
-  storageBucket: "zealthy-test.firebasestorage.app",
-  messagingSenderId: "12639225378",
-  appId: "1:12639225378:web:3338959a11e142e73bec7b",
-  measurementId: "G-ZWRGYDCMLR"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
